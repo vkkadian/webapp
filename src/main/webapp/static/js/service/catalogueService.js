5c44d5299f537023966b898b7c0af65f325d3ef9
vkkadian <vkkadian@yahoo.com>
2016-05-15 00:05:35 +0530

    service urls changed to point to restful service at openshift

--- src/main/webapp/static/js/service/catalogueService.js
+++ src/main/webapp/static/js/service/catalogueService.js
@@ -4,7 +4,8 @@ var catalogueService = angular.module('catalogueService', ['ngResource']);
 App.factory('Catalogue', ['$resource', function ($resource) {
 	//$resource() function returns an object of resource class
     return $resource(
-    		'/webapp/static/data/data.json', {},
+    		//'/webapp/static/data/data.json', {},
+    		'http://catalogue-excercise.rhcloud.com/items',{},
     		{
     		    query: {
     		        method: 'GET',
